44833683e4e3f02b9cae2fffc8794f0211abfb5a
Tongjie <pop32132@sina.com>
2016-05-05 18:08:30 -0400

    add lattice constant support for conversion program

--- genx-conv.py
+++ genx-conv.py
@@ -1,10 +1,14 @@
-
 #!/usr/bin/python2
-# To use this script, open terminal, type "python thisscript.py xyzfile", then you will get the output in terminal
+# To use this script, open terminal, type "python thisscript.py xyzfile", then you will get the output in terminal.
+# First parameter as xyz filename, second name as lattice constant
 
 from __future__ import print_function
 import csv,sys
-with open(sys.argv[1]) as xyzfile:
+filename=sys.argv[1]
+lc=float(sys.argv[2])
+with open(filename) as xyzfile:
+    n=1
     for line in xyzfile.readlines()[3:]:
         array = line.split()
-        print("add_atom(" + "id='"+array[0]+"'"+","+"el='"+array[0]+"'"+','+'x='+array[1]+','+'y='+array[2] + ',' + 'z='+array[3] +")")
+        print("add_atom(" + "id='"+array[0]+str(n)+"'"+","+"el='"+array[0]+"'"+','+'x='+str(float(array[1])/lc)+','+'y='+str(float(array[2])/lc) + ',' + 'z='+str(float(array[3])/lc) +")")
+        n +=1
